381015e6120fe9049895fc0648f9be72ae61a3dd
Anthony Gabriel $_$ <7anthonygabriel@gmail.com>
2025-08-29 10:42:48 -0300

    Add files via upload

---
+++ aula 09/ensaios.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+    //struct é tipo um container que ajuda a otimizar funções do código que agrupa dados relacionados
+struct Aluno{
+    string nome;
+    int idade;
+    float nota;
+
+    //o construtor coloca as propriedades de valor na estrutura
+    Aluno (string nome, int idade, float nota) {
+        this->nome = n;
+        this->idade = i;
+        this->nota = nt;
+    }
+
+    void mostrardados () {
+        cout << "Informe seu nome: ";
+        cin >> aluno.nome;
+
+        cout << "Informe a sua idade: ";
+        cin >> aluno.idade;
+
+        cout << "Informe a sua nota: ";
+        cin >> aluno.nota;
+    }
+
+int main () {
+    Aluno turma [3] = {
+
+    for (int i = 0; i < 3; i++) {
+        cout << "nome";
+        getline (cin, turma [i].nome);
+
+        cin.ignora();
+    }
+    
+}
+
+    return 0;
+};
